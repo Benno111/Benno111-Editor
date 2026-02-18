aeff95328363b9d6f6864332ad425e3785f7067e
Benno111 <80102205+Benno111@users.noreply.github.com>
2026-02-18 17:35:17 +0100

    Update Build.bat

--- Build.bat
+++ Build.bat
@@ -1,4 +1,3 @@
 @echo off
-cd C:\Users\Benno111\Desktop\tiny_c_editor
 windres resource.rc -O coff -o resource.o
 gcc -O2 -Wall -Wextra -std=c11 -mwindows editor.c resource.o -o editor.exe -lcomdlg32 -ld2d1 -luuid -lole32
